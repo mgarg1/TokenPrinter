1ddee272798447ac869aa989cdbbbfc167246a64
mgarg1 <mrmohitgarg1990@gmail.com>
2020-01-05 14:18:59 +0530

    removed unnecessary image

--- tests/test_tokenMgr.py
+++ tests/test_tokenMgr.py
@@ -1,17 +1,29 @@
 import sys
 sys.path.insert(0, '../')
+import os
 
 from datetime import datetime,timedelta
-from tokenMgr import getNextTokenNumber,writeToken,DATE_FORMAT,TOKEN_START,TOKEN_DELTA 
+from tokenMgr import getNextTokenNumber,writeToken,DATE_FORMAT,TOKEN_START,TOKEN_DELTA
+from tokenMgr import LAST_TOKEN_FILENAME, LAST_DATE_FILENAME
+
+
+def cleanup():
+    os.remove(LAST_TOKEN_FILENAME)
+    os.remove(LAST_DATE_FILENAME)
+
 
 # Test to check if two consecutive getNextTokenNumber return right results
 def testToken1():
     token = getNextTokenNumber()
     assert token != None, "token number cannot be None"
     writeToken(token)
+    
+    assert os.path.exists(LAST_TOKEN_FILENAME), "LAST_TOKEN_FILENAME doesn't exist"
+    assert os.path.exists(LAST_DATE_FILENAME), "LAST_DATE_FILENAME doesn't exist"
+
     token2 = getNextTokenNumber()
     assert token2 == token+TOKEN_DELTA, "token number is not updated on write"
-
+    cleanup()
 
 # Test to check if reset of token number works on day change
 def testToken2():
@@ -28,7 +40,7 @@ def testToken2():
     
     token2 = getNextTokenNumber(nextDay)
     assert token2 == TOKEN_START,"nextDay token resetting not working"
-
+    cleanup()
 
 testToken1()
 testToken2()
